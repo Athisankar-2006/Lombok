9422e0c9c2da23ad25054a9dafceef3d7accd675
Athisankar-2006 <athisankar2k6@gmail.com>
2026-04-26 22:41:03 +0530

    manual object created

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,15 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Student student=new Student();
+        student.setAge(23);
+        System.out.println(student.getAge());
+        System.out.println(student.toString());
     }
 }
